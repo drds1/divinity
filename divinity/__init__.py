10f890099fc77671dbb19ee893d0c1e4e3b3b61d
dstarkey23 <david.starkey@vortexa.com>
2020-08-11 18:34:59 +0100

    add pypi format - not yet live

---
+++ divinity/__init__.py
@@ -0,0 +1,68 @@
+import numpy as np
+import pandas as pd
+
+def gen_season(N,periods = [10,20],sine_amplitudes = [1,1],cosine_amplitudes = [0,0]):
+    '''
+    generate sinusoidal features
+    :param N:
+    :param periods:
+    :param sine_amplitudes:
+    :param cosine_amplitudes:
+    :return:
+    '''
+    features = {}
+    t = np.arange(N)
+    X = np.zeros(N)
+    for p, sa, ca in zip(periods,sine_amplitudes, cosine_amplitudes):
+        s = np.sin(2*np.pi/p * t)
+        X+= sa * s
+        c = np.cos(2 * np.pi / p * t)
+        X += ca * c
+        features['sin P=' + str(p)] = s
+        features['cos P=' + str(p)] = c
+    return {'features':pd.DataFrame(features),'target':X}
+
+def gen_trend(N, coef = [0,1,2], amplitudes = [1,0.5,0.2]):
+    '''
+    generate trend features
+    :param N:
+    :param coef:
+    :param amplitudes:
+    :return:
+    '''
+    features = {}
+    t = np.arange(N)
+    X = np.zeros(N)
+    for order, amp in zip(coef,amplitudes):
+        f = np.array(t**order,dtype='float')
+        X+= amp*f
+        features['trend order '+str(order)] = f
+    return {'features':pd.DataFrame(features),'target':X}
+
+
+
+def gen_fake(N, trend_order,
+             trend_amplitudes,
+             seasonal_periods,
+             seasonal_amplitudes_sine,
+             seasonal_amplitudes_cosine):
+    t = np.arange(N)
+    features = {}
+
+    #calculate trend features
+    trend = gen_trend(N,coef = list(range(trend_order + 1)),
+                      amplitudes=trend_amplitudes)
+    features_trend, y_trend = trend['features'], trend['target']
+
+    #calculate sine seasonal features
+    season = gen_season(N, periods=seasonal_periods,
+                        sine_amplitudes=seasonal_amplitudes_sine,
+                        cosine_amplitudes=seasonal_amplitudes_cosine)
+    features_season, y_season = season['features'], season['target']
+
+    #combine and output features and target
+    features_tot = pd.concat([features_trend, features_season], axis=1)
+    y_tot = y_trend + y_season
+    return{'features':features_tot,'target':y_tot}
+
+
---
+++ setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""A Nose plugin to support IPython doctests.
+"""
+#upload to pip
+#pip install .
+#python setup.py sdist
+#twine upload dist/divinity-0.1.tar.gz
+
+from setuptools import setup
+
+setup(name='divinity',
+      version='0.1',
+      url='https://github.com/dstarkey23/divinity',
+      author='David Starkey',
+      author_email='davidstarkey29@gmail.com',
+      description = 'Timeseries forecast package',
+      license = 'Apache',
+      packages=['divinity',],
+      entry_points = {},
+      install_requires=['pandas','numpy','matplotlib','statsmodels']
+      )
+
+
+#setup(name='pycecream',
+#      version='1.5.2',
+#      description='python implementation of the cream accretion disc fitting code '
+#                  'https://academic.oup.com/mnras/article-abstract/456/2/1960/1066664?redirectedFrom=PDF'
+#      ,
+#      long_description= 'add pycecream.dream light curve merging feature'
+#      ,url='https://github.com/dstarkey23/pycecream',
+#      author='dstarkey23',
+#      author_email='ds207@st-andrews.ac.uk',
+#      license='MIT',
+#      packages=['pycecream'],
+#      package_data={'': ['creaminpar.par','cream_f90.f90']},
+#      install_requires=[
+#      'pandas',
+#      'numpy',
+#      'matplotlib',
+#      'scipy',
+#      'astropy_stark',
+#      'glob3',
+#      'PyQt5',
+#      'corner',
+#      'seaborn'
+#      ],
+#extras_require={
+#        'tests': [
+#            'nose2==0.9.1',
+#            'pre-commit==1.20.0',
+#            'flake8==3.7.9',
+#            'pydoc-markdown==2.0.4',
+#            'tabulate==0.8.5',
+#            'six==1.12.0'
+#        ]
+#    },
+#      zip_safe=False)
