69bc6211d17fe530a77a9041bc8ac6719f6d588a
dstarkey23 <david.starkey@vortexa.com>
2020-08-16 17:43:05 +0100

    add updated example scratch2

--- scratch/scratch2.py
+++ scratch/scratch2.py
@@ -2,19 +2,29 @@ import numpy as np
 import pandas as pd
 import divinity as dv
 import matplotlib.pyplot as plt
-
+import time
 
 if __name__ == '__main__':
-
+    np.random.seed(123456)
     #now test the class and the new auto assigner
     N = 120
     Ntest = 100
     t = np.arange(N)
     y_test = 0.1*t + np.sin(2*np.pi/20*t) + np.random.randn(N)*0.5
+    t1 = time.time()
     dfc = dv.divinity(forecast_length=N - Ntest,
+                      seasonal_periods=list(np.arange(1,int(N/2))),
                       confidence_interval=70.)
     dfc.fit(y_test[:Ntest])
     y_forecast = dfc.predict()
+    t2 = time.time()
+    print('fit report')
+    print('fit time...',t2 - t1)
+    print('optimisation report...')
+    print(str(len(dfc.input_features.columns))+' input features')
+    print(str(len(dfc.features.columns))+' chosen features')
+    print(dfc.features.columns)
+
     fig = plt.figure()
     ax1 = fig.add_subplot(111)
     ax1.plot(t, y_test,label='True',color='k')
@@ -29,4 +39,4 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
 
-    #y_forecast_2 = dfc.forecast(steps= 50)
+    y_forecast_2 = dfc.forecast(steps= 50)
