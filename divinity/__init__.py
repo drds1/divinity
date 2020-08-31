9b31776c7bff9dcb5d4f9ca8ab60b08c6dc9c4a3
dstarkey23 <david.starkey@vortexa.com>
2020-08-31 13:50:06 +0100

    add docs folder for documentation

--- divinity/__init__.py
+++ divinity/__init__.py
@@ -1,3 +1,22 @@
+"""
+Divinity is a custom timeseries forecast library. This is currently
+ in development and designed to automate and
+ generalise (as much as possible) the process of
+ generating any timeseries forecast.
+
+
+ ![Test Image 1](https://github.com/dstarkey23/divinity/blob/master/notebooks/test_divinity_forecast.png)
+
+ This is an RnD project aiming to start simply and
+ build additional functionality as needed. Please see
+ the issues and project page for a backlog of functionality
+ to be added to the package.
+
+    pydoc -w foo
+
+"""
+
+
 import numpy as np
 import pandas as pd
 import sklearn.linear_model
